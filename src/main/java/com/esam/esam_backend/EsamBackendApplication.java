9d4e028dfba60aae4c2bf037a0b358c33307afe9
MadoScientista <samuel.cortes.n@gmail.com>
2026-09-01 15:30:25 -0400

    Primer commit

---
+++ src/main/java/com/esam/esam_backend/EsamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esam.esam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esam/esam_backend/EsamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esam.esam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
